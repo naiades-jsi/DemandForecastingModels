9d6d941581f715907fef94f6a61fc9f1909f4ab5
Joao Costa <joaocostamat@gmail.com>
2021-10-27 11:08:20 +0100

    Updated Model

--- src/model.py
+++ src/model.py
@@ -50,19 +50,11 @@ class LSTM_model():
         print("ftr_vector:" + str(ftr_vector))
 
         timestamp = message_value["timestamp"]
-
-<<<<<<< HEAD
-        predicted_demand = list(self.nn.predict(np.atleast_2d(ftr_vector))[0])
-        output_dictionary = {"timestamp": message_value['timestamp'], 
-        "value": str(predicted_demand), 
-        "horizon": str(self.horizon)}
-=======
         predicted_demand = [float(k) for k in self.nn.predict(np.atleast_2d(ftr_vector))[0]]
         output_dictionary = {"timestamp": message_value['timestamp'], 
         "value": predicted_demand, 
         "horizon": self.horizon,
         "prediction_time": time.time()}
->>>>>>> 10d256de42ff1e47b13d90a7b95242e8952e61a0
 
         for output in self.outputs:
             output.send_out(timestamp=timestamp,
