84822ebd3a94c9cf251b5c9eaac20f1adb4061ef
Joao Costa <joaocostamat@gmail.com>
2021-11-10 12:31:04 +0000

    "Added code for running on cpu-only environment"

--- src/model.py
+++ src/model.py
@@ -85,23 +85,24 @@ class LSTM_model():
             y_test = test[timesteps:, :]
             return X_train, y_train, X_test, y_test
         
-        [self.training_dataf, self.testing_dataf] = partitionSet(model_structure["test_size"], self.training_data, 100-model_structure["test_size"]*100)
-        [self.training_X_data, self.training_Y_data, self.testing_X_data, self.testing_Y_data] = Dataset(self.training_dataf, self.testing_dataf, 24)
-        self.nn = Sequential()
-        self.nn.add(Masking(mask_value=0., input_shape=(model_structure["n_of_timesteps"], model_structure["num_features"])))
-        self.nn.add(LSTM(1, activation = 'tanh', input_shape = (model_structure["n_of_timesteps"], model_structure["num_features"]), return_sequences=True))
-        self.nn.add(Dropout(model_structure["dropout"]))
-        self.nn.add(LSTM(model_structure["n_of_neurons"]))
-        self.nn.add(Dropout(model_structure["dropout"]))
-        self.nn.add(Dense(1))
-        self.nn.compile(loss = 'mse', optimizer='adam')
+        with tf.device("CPU:0"):
+            [self.training_dataf, self.testing_dataf] = partitionSet(model_structure["test_size"], self.training_data, 100-model_structure["test_size"]*100)
+            [self.training_X_data, self.training_Y_data, self.testing_X_data, self.testing_Y_data] = Dataset(self.training_dataf, self.testing_dataf, 24)
+            self.nn = Sequential()
+            self.nn.add(Masking(mask_value=0., input_shape=(model_structure["n_of_timesteps"], model_structure["num_features"])))
+            self.nn.add(LSTM(1, activation = 'tanh', input_shape = (model_structure["n_of_timesteps"], model_structure["num_features"]), return_sequences=True))
+            self.nn.add(Dropout(model_structure["dropout"]))
+            self.nn.add(LSTM(model_structure["n_of_neurons"]))
+            self.nn.add(Dropout(model_structure["dropout"]))
+            self.nn.add(Dense(1))
+            self.nn.compile(loss = 'mse', optimizer='adam')
 
-        X_ = ma.filled(self.training_X_data,0)
-        Y_ = ma.filled(self.training_Y_data,0)
+            X_ = ma.filled(self.training_X_data,0)
+            Y_ = ma.filled(self.training_Y_data,0)
 
-        self.model = self.nn.fit(X_, Y_, epochs = model_structure["epochs"], batch_size = model_structure["batch_size"],
-                        validation_split = model_structure["validation_split"], shuffle = False, verbose = 0)
-        self.save_model(self.model_name)
+            self.model = self.nn.fit(X_, Y_, epochs = model_structure["epochs"], batch_size = model_structure["batch_size"],
+                            validation_split = model_structure["validation_split"], shuffle = False, verbose = 0)
+            self.save_model(self.model_name)
 
     def save_model(self, filename):
         self.nn.save("models/" + filename + "_LSTM")
